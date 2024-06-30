83768387937406fd6d78a57767444b04ff26b63d
Orisabiyi <orisabiyidavid@gmail.com>
2024-06-30 07:08:37 +0100

    created a sidebar component and included it in the Applayout component

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,9 +1,11 @@
-import AppNav from "../components/AppNav";
+import Sidebar from "../components/Sidebar";
+
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
-    <div>
-      <AppNav />
+    <div className={styles.app}>
+      <Sidebar />
       <p>App</p>
     </div>
   );
