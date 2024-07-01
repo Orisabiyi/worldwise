90c9a63cf28ca6f32bd67d7f22330d07f1d8bc9e
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-01 11:51:51 +0100

    implemented a nested route for the sidebar navigation

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,13 @@ function App() {
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+        </Route>
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
