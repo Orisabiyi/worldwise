cc8a691fc3541eae77a3381a1d3707798f00d57e
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-02 05:20:32 +0100

    added the visuals for the countrylist

---
+++ src/components/CountryList.jsx
@@ -0,0 +1,21 @@
+import CountryItem from "./CountryItem";
+import styles from "./CountryList.module.css";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CountryList({ cities, isLoading }) {
+  if (isLoading) <Spinner />;
+
+  if (!cities.length)
+    <Message message="Add your first city by clicking on a city on the map" />;
+
+  return (
+    <ul className={styles.CountryList}>
+      {cities.map((city) => (
+        <CountryItem city={city} key={city.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountryList;
