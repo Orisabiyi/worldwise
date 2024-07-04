e0efb9c29d8df3daf846625820486849b051089a
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-04 04:15:50 +0100

    render list of countries dynamically from array removing duplication

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -9,9 +9,15 @@ function CountryList({ cities, isLoading }) {
   if (!cities.length)
     <Message message="Add your first city by clicking on a city on the map" />;
 
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
   return (
     <ul className={styles.countryList}>
-      {cities.map((city) => (
+      {countries.map((city) => (
         <CountryItem city={city} key={city.id} />
       ))}
     </ul>
