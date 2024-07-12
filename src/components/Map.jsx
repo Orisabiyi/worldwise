aca8c180637ec6f39532d9402899d46526efea08
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-12 11:33:39 +0100

    retrieved values stored in the url using useSearhParam

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,22 @@
+import { useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParam, setSearchParam] = useSearchParams();
+  const lat = searchParam.get("lat");
+  const lng = searchParam.get("lng");
+
+  return (
+    <div className={styles.mapContainer}>
+      <h1>Map</h1>
+      <h1>
+        lat: {lat}, lng: {lng}
+      </h1>
+      <button onClick={() => setSearchParam({ lat: 20, lng: 50 })}>
+        Change Position
+      </button>
+    </div>
+  );
 }
 
 export default Map;
