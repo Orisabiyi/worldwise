431bcfae93d926d3e946a45efd120c69521b2212
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-22 15:15:41 +0100

    chore: Update BASE_URL in CitiesProvider to use environment variable

--- src/contexts/CitiesProvider.jsx
+++ src/contexts/CitiesProvider.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect, createContext } from "react";
 
+const BASE_URL = "http://localhost:8000";
 const citiesContext = createContext();
 
 function CitiesProvider({ children }) {
