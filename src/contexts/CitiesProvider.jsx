693bf4b00738dce5af15fb3326ec3f641a9dfa69
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-22 15:14:28 +0100

    chore: Refactor CitiesProvider to fetch and provide cities data

--- src/contexts/CitiesProvider.jsx
+++ src/contexts/CitiesProvider.jsx
@@ -1,9 +1,34 @@
-import { createContext } from "react";
+import { useState, useEffect, createContext } from "react";
 
 const citiesContext = createContext();
 
 function CitiesProvider({ children }) {
-  return <citiesContext.Provider>{children}</citiesContext.Provider>;
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function getCountries() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+
+        setCities(data);
+      } catch (err) {
+        alert(err);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    getCountries();
+  }, []);
+
+  return (
+    <citiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </citiesContext.Provider>
+  );
 }
 
 export { CitiesProvider };
