21741cf9671a56453b5306a0fd84c18e569ada16
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-23 10:46:23 +0100

    feat: include a dynamic map that display cities on a leaflet map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,22 +1,38 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import styles from "./Map.module.css";
+import { useState } from "react";
+import { useCities } from "../contexts/CitiesProvider";
 
 function Map() {
   const navigate = useNavigate();
 
   const [searchParam, setSearchParam] = useSearchParams();
-  const lat = searchParam.get("lat");
-  const lng = searchParam.get("lng");
+  const [mapPosition, setMapPosition] = useState([40, 0]);
+  const { cities } = useCities();
+  // const lat = searchParam.get("lat");
+  // const lng = searchParam.get("lng");
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
-      <h1>Map</h1>
-      <h1>
-        lat: {lat}, lng: {lng}
-      </h1>
-      <button onClick={() => setSearchParam({ lat: 20, lng: 50 })}>
-        Change Position
-      </button>
+      <MapContainer
+        center={mapPosition}
+        zoom={13}
+        scrollWheelZoom={true}
+        className={styles.map}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker position={[city.position.lat, city.position.lng]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
