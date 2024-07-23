80e6451f51bb925360dd4eb580db22e11cbe97ad
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-23 03:09:02 +0100

    refactor: Use useEffect hook to fetch city data in City component

--- src/components/City.jsx
+++ src/components/City.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import Button from "./Button";
 import styles from "./City.module.css";
 import { useCities } from "../contexts/CitiesProvider";
+import { useEffect } from "react";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
@@ -15,44 +16,45 @@ function City() {
   const { id } = useParams();
   const { currentCity, getCity } = useCities();
 
+  useEffect(
+    function () {
+      getCity(id);
+    },
+    [id]
+  );
+
   const { cityName, emoji, date, notes } = currentCity;
 
   return (
     <div className={styles.city}>
       city {id}
-      {/* <div className={styles.row}>
+      <div className={styles.row}>
         <h6>City name</h6>
         <h3>
           <span>{emoji}</span> {cityName}
         </h3>
       </div>
-
       <div className={styles.row}>
         <h6>You went to {cityName} on</h6>
         <p>{formatDate(date || null)}</p>
       </div>
-
       {notes && (
         <div className={styles.row}>
           <h6>Your notes</h6>
-          <p>{notes}</p> 
+          <p>{notes}</p>
         </div>
       )}
-
-      // <div className={styles.row}>
-      //   <h6>Learn more</h6>
-      //   <a
-      //     href={`https://en.wikipedia.org/wiki/${cityName}`}
-      //     target="_blank"
-      //     rel="noreferrer"
-      //   >
-      //     Check out {cityName} on Wikipedia &rarr;
-      //   </a>
-      // </div>
-
-      // <div>
-      //   <Button />
-      // </div> */}
+      <div className={styles.row}>
+        <h6>Learn more</h6>
+        <a
+          href={`https://en.wikipedia.org/wiki/${cityName}`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Check out {cityName} on Wikipedia &rarr;
+        </a>
+      </div>
+      <div></div>
     </div>
   );
 }
