23d8a74b8a07a21fca4beb467c713344efb4d53f
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-25 08:09:05 +0100

    refactor: chnaged the popup text of the map marker

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -31,7 +31,8 @@ function Map() {
             key={city.id}
           >
             <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
             </Popup>
           </Marker>
         ))}
