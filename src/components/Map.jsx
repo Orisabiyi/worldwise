bdc2eae927a1873533dd0e958229986213fd7660
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-25 10:49:39 +0100

    feat: Add dynamic map with leaflet library and display cities

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,23 +1,30 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import styles from "./Map.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useCities } from "../contexts/CitiesProvider";
 
 function Map() {
   const navigate = useNavigate();
 
-  const [searchParam, setSearchParam] = useSearchParams();
+  const [searchParam] = useSearchParams();
   const [mapPosition, setMapPosition] = useState([40, 0]);
   const { cities } = useCities();
-  // const lat = searchParam.get("lat");
-  // const lng = searchParam.get("lng");
+  const mapLat = searchParam.get("lat");
+  const mapLng = searchParam.get("lng");
+
+  useEffect(
+    function () {
+      if (mapLat && mapLng) setMapPosition([mapLat, mapLng]);
+    },
+    [mapLat, mapLng]
+  );
 
   return (
     <div className={styles.mapContainer} onClick={() => navigate("form")}>
       <MapContainer
         center={mapPosition}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
         className={styles.map}
       >
@@ -36,9 +43,16 @@ function Map() {
             </Popup>
           </Marker>
         ))}
+        <SetMapView position={mapPosition} />
       </MapContainer>
     </div>
   );
+}
+
+function SetMapView({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
 }
 
 export default Map;
