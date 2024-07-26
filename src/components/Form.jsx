a058e6627c1bfea508939685dc58c0f83a89af4c
Orisabiyi <orisabiyidavid@gmail.com>
2024-07-26 14:48:12 +0100

    refactor: Add new city to cities.json dynaically and update Form component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,6 +12,7 @@ import Button from "./Button";
 import BackButton from "./BackButton";
 import Message from "./Message";
 import Spinner from "./Spinner";
+import { useNavigate } from "react-router-dom";
 
 const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
@@ -25,7 +26,8 @@ export function convertToEmoji(countryCode) {
 
 function Form() {
   const [lat, lng] = useUrlPosition();
-  const { createCity } = useCities();
+  const { createCity, isLoading } = useCities();
+  const navigate = useNavigate("");
 
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
@@ -84,6 +86,7 @@ function Form() {
     };
 
     await createCity(newCity);
+    navigate("/app");
   }
 
   if (isLoadingGeocoding) <Spinner />;
@@ -93,7 +96,10 @@ function Form() {
   if (geocodingError) return <Message message={geocodingError} />;
 
   return (
-    <form className={styles.form} onSubmit={handleSubmit}>
+    <form
+      className={`${styles.form} ${isLoading ? styles.loading : ""}`}
+      onSubmit={handleSubmit}
+    >
       <div className={styles.row}>
         <label htmlFor="cityName">City name</label>
         <input
